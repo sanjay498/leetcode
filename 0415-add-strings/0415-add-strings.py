dbc9e68763f30a7d989a7ba6e4e57a109d5b1efa
sanjay1157 <85265492+sanjay498@users.noreply.github.com>
2025-06-16 14:45:11 +0530

    Time: 0 ms (100%), Space: 12.6 MB (48.58%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        val1=int(num1)
+        val2=int(num2)
+        total=val1+val2
+        k=str(total)
+        return k
+        
